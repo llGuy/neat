ccfbab72f50d9dfe07b34c1ee56976d5dc8aaaed
Luc <luc.rosenzweig@yahoo.com>
2020-06-21 09:46:06 +0100

    Initial commit

---
+++ source/main.cpp
@@ -0,0 +1,9 @@
+#include <stdint.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+int32_t main(
+    int argc,
+    char *argv[]) {
+    return 0;
+}
---
+++ source/neat.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+// Structure which holds information on ALL the genes / gene connections that exist atm
+struct neat_t {
+    
+};

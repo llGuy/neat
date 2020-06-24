3a527e763e5ff4e2b87c36f818d411efaa1909a0
Luc <luc.rosenzweig@yahoo.com>
2020-06-24 12:20:52 +0100

    Added bird

--- source/main.cpp
+++ source/main.cpp
@@ -22,7 +22,8 @@ struct bird_t {
 
 static struct {
     // neat_universe_t universe;
-    bird_t bird;
+    uint32_t bird_count;
+    bird_t birds[100];
 
     // Distance between the pipes
     float pipe_distance;
@@ -31,6 +32,8 @@ static struct {
     // Always 4 pipes at a time
     float pipe_positions[4];
     float opening_centres[4];
+
+    float dt;
 } game;
 
 static float s_rand_01() {
@@ -39,12 +42,19 @@ static float s_rand_01() {
 
 static void s_update_game() {
     for (uint32_t i = 0; i < 4; ++i) {
-        game.pipe_positions[i] -= 0.001f;
+        game.pipe_positions[i] -= game.dt * 0.1f;
 
         if (game.pipe_positions[i] <= -1.0f) {
             game.pipe_positions[i] = 1.0f;
             game.opening_centres[i] = s_rand_01() * 1.6f + 0.2f - 1.0f;
         }
+    }
+
+    for (uint32_t i = 0; i < game.bird_count; ++i) {
+        bird_t *bird = &game.birds[i];
+        bird->velocity_y -= game.dt * 3.0f;
+
+        bird->current_y += bird->velocity_y * game.dt;
     }
 }
 
@@ -60,11 +70,26 @@ static void s_render_game() {
         glVertex2f(game.pipe_positions[i], game.opening_centres[i] - game.pipe_opening_size / 2.0f);
     }
     glEnd();
+
+    glBegin(GL_POINTS);
+    for (uint32_t i = 0; i < game.bird_count; ++i) {
+        bird_t *bird = &game.birds[i];
+        glColor4f(bird->shade, bird->shade, bird->shade, bird->shade);
+        glVertex2f(-0.9f, bird->current_y);
+    }
+    glEnd();
 }
 
 static void s_game_init() {
     // There will be 20 birds
     // universe_init(&game.universe, 20, 4, 2);
+
+    game.bird_count = 1;
+    for (uint32_t i = 0; i < game.bird_count; ++i) {
+        game.birds[i].current_y = 0.0f;
+        game.birds[i].shade = 1.0f;
+        game.birds[i].velocity_y = 0.0f;
+    }
 
     game.pipe_opening_size = 0.3f;
     game.pipe_distance = 0.5f;
@@ -109,6 +134,10 @@ static void s_window_key_callback(
 
         case GLFW_KEY_G: {
             mutate_add_gene(&genome, &neat);
+        } break;
+
+        case GLFW_KEY_SPACE: {
+            game.birds[0].velocity_y = 1.0f;
         } break;
         }
     }
@@ -190,7 +219,11 @@ int32_t main(
 
     s_game_init();
 
+    game.dt = 0.0f;
+
     while(running) {
+        float frame_time = glfwGetTime();
+
         gl_begin_frame();
 
         glClear(GL_COLOR_BUFFER_BIT);
@@ -234,6 +267,9 @@ int32_t main(
         s_update_game();
 
         gl_end_frame();
+
+        frame_time = glfwGetTime() - frame_time;
+        game.dt = frame_time;
     }
 
     gl_context_terminate();
